786cf1d63db8bfcec9d3c0217eedac281e56f4d5
yapianyu <jgfang725@gmail.com>
2012-11-28 01:05:16 +0800

    write to mongodb

--- crawler/pptv_crawler.py
+++ crawler/pptv_crawler.py
@@ -6,6 +6,7 @@ Created on Nov 25, 2012
 
 from crawler_base import CrawlerBase
 from lxml.html import fromstring
+from pymongo.connection import Connection
 import time
 import urllib2
 
@@ -16,6 +17,9 @@ class PPTVCrawler(CrawlerBase):
 
     def __init__(self, sleep_time):
         self.__sleep_time = sleep_time
+        self.__connection = Connection()
+        self.__db = self.__connection['videocabindb']
+        self.__collection = self.__db['movies']
 
     def crawl(self):
         page_index = 172
@@ -35,6 +39,8 @@ class PPTVCrawler(CrawlerBase):
                 movie_title_element = movie_element.xpath('./p[@class="txt"]/a')[0]
                 movie_cast_elements = movie_element.xpath('./p[@class="p_actor"]/a')
                 movie_image_element = movie_element.xpath('./p[@class="pic"]/a/img')[0]
+                self.__collection.update({'title': movie_title_element.text}, {'$set': {'pptv.link': movie_title_element.attrib['href']}, '$inc': {'pptv.play_times': 0}}, True)
+
                 print movie_index, movie_title_element.text, movie_title_element.attrib['href'],
                 for movie_cast_element in movie_cast_elements:
                     print movie_cast_element.text,
