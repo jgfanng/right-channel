fc4d89d88206fb820a7aa4c1320dafc308656770
yapianyu <jgfang725@gmail.com>
2013-01-07 01:24:20 +0800

    small change

--- crawlers/iqiyi_crawler.py
+++ crawlers/iqiyi_crawler.py
@@ -12,6 +12,7 @@ from crawlers.utils.mongodb import movie_store_collection
 from difflib import SequenceMatcher
 from lxml.html import fromstring
 from pymongo.errors import PyMongoError
+from threading import Thread
 from urllib2 import HTTPError, URLError
 import datetime
 import time
@@ -29,11 +30,15 @@ class IQIYICrawler(object):
         self.__total_movies_crawled = 0
 
     def start_crawl(self):
+        # this thread is used to manually match movies which are not indexed by mongodb
+        matcher = Thread(target=self.__match_manually)
+        matcher.start()
+        # start to crawl iqiyi
         while True:
             IQIYICrawler.logger.info('==========Start to crawl iqiyi movies==========')
             self.__start_crawl()
             IQIYICrawler.logger.info('==========Finish crawling iqiyi movies=========')
-            IQIYICrawler.logger.info('==========Totally movies(%s)===================' % self.__total_movies_crawled)
+            IQIYICrawler.logger.info('==========Total movies: %s===================' % self.__total_movies_crawled)
 
     def __start_crawl(self):
         '''
@@ -75,10 +80,10 @@ class IQIYICrawler(object):
                         if not movie_year:
                             IQIYICrawler.logger.warning('Movie year not found on playing page <%s>. Please check whether the xpath has changed' % playing_page_url)
                     except HTTPError, e:
-                        IQIYICrawler.logger.error('Server cannot fulfill the request <%s %s %s>' % (playing_page_url, e.code, e.msg))
+                        IQIYICrawler.logger.error('Server cannot fulfill the request <%s> <%s> <%s>' % (playing_page_url, e.code, e.msg))
                         continue
                     except URLError, e:
-                        IQIYICrawler.logger.error('Failed to reach server <%s %s>' % (playing_page_url, e.reason))
+                        IQIYICrawler.logger.error('Failed to reach server <%s> <%s>' % (playing_page_url, e.reason))
                         continue
                     except Exception, e:
                         IQIYICrawler.logger.error('%s <%s>' % (e, playing_page_url))
@@ -99,10 +104,10 @@ class IQIYICrawler(object):
                             IQIYICrawler.logger.warning('Movie title not found on introduction page <%s>. Please check whether the xpath has changed' % intro_page_url)
                             continue  # skip this movie
                     except HTTPError, e:
-                        IQIYICrawler.logger.error('Server cannot fulfill the request <%s %s %s>' % (intro_page_url, e.code, e.msg))
+                        IQIYICrawler.logger.error('Server cannot fulfill the request <%s> <%s> <%s>' % (intro_page_url, e.code, e.msg))
                         continue
                     except URLError, e:
-                        IQIYICrawler.logger.error('Failed to reach server <%s %s>' % (intro_page_url, e.reason))
+                        IQIYICrawler.logger.error('Failed to reach server <%s> <%s>' % (intro_page_url, e.reason))
                         continue
                     except Exception, e:
                         IQIYICrawler.logger.error('%s <%s>' % (e, intro_page_url))
@@ -112,26 +117,29 @@ class IQIYICrawler(object):
                     movie_definition_elements = movie_element.xpath('./a/span[@class="cqBg"]')
                     movie_definition = u'超清' if movie_definition_elements else u'一般'
 
+                    self.__total_movies_crawled += 1
+                    IQIYICrawler.logger.info('Crawled movie #%s <%s> <%s> <%s>' % (self.__total_movies_crawled, movie_year, movie_title, movie_definition))
+
                     #----------Save to Mongodb-------------------------------
                     try:
                         result = movie_store_collection.find_and_modify(query={'year': movie_year, '$or': [{'title': movie_title}, {'alt_titles': movie_title}]},  # Caution: if 'movie_year' is None, mongodb will return records with no 'year' field or its value is 'null'
                                                                         update={'$set': {'iqiyi.title': movie_title, 'iqiyi.similarity': 1.0, 'iqiyi.link': playing_page_url, 'iqiyi.definition': movie_definition, 'iqiyi.last_updated': datetime.datetime.utcnow()}, '$inc': {'iqiyi.play_times': 0}},  # Caution: if 'play_times' does not exist, mongodb will automatically set its value to 0
                                                                         fields={'_id': 1})
-                        if not result:
+                        if result:
+                            IQIYICrawler.logger.info('Find a match with douban <%s>' % movie_title)
+                        else:
                             self.__unmatched_queue.put((movie_year, movie_title, playing_page_url, movie_definition))
-
-                        self.__total_movies_crawled += 1
-                        IQIYICrawler.logger.info('Crawled movie #%s <%s %s %s>' % (self.__total_movies_crawled, movie_year, movie_title, movie_definition))
+                            IQIYICrawler.logger.info('Cannot find a match with douban <%s>' % movie_title)
 
                     except PyMongoError, e:
-                        IQIYICrawler.logger.error('Mongodb error <%s %s>' % (e, playing_page_url))
+                        IQIYICrawler.logger.error('Mongodb error <%s> <%s>' % (e, playing_page_url))
                         continue
                     #--------------------------------------------------------
 
             except HTTPError, e:
-                IQIYICrawler.logger.error('Server cannot fulfill the request <%s %s %s>' % (movie_list_page_url, e.code, e.msg))
+                IQIYICrawler.logger.error('Server cannot fulfill the request <%s> <%s> <%s>' % (movie_list_page_url, e.code, e.msg))
             except URLError, e:
-                IQIYICrawler.logger.error('Failed to reach server <%s %s>' % (movie_list_page_url, e.reason))
+                IQIYICrawler.logger.error('Failed to reach server <%s> <%s>' % (movie_list_page_url, e.reason))
             except Exception, e:
                 IQIYICrawler.logger.error('%s <%s>' % (e, movie_list_page_url))
 
@@ -167,7 +175,7 @@ class IQIYICrawler(object):
 
         return movie_title_elements[0].text.strip() if movie_title_elements and movie_title_elements[0].text else None
 
-    def match_manually(self):
+    def __match_manually(self):
         '''
         Traverse mongodb and manually match movies.
         '''
@@ -191,10 +199,12 @@ class IQIYICrawler(object):
                             max_score = score
                             similar_movie = movie_info
 
-                if max_score > 0.0:
+                if max_score > 0.0 and similar_movie:
                     movie_store_collection.update({'id': similar_movie['id']},
                                                   {'$set': {'iqiyi.title': movie_title, 'iqiyi.similarity': max_score, 'iqiyi.link': playing_page_url, 'iqiyi.definition': movie_definition, 'iqiyi.last_updated': datetime.datetime.utcnow()}, '$inc': {'iqiyi.play_times': 0}})
-                    IQIYICrawler.logger.debug()
+                    IQIYICrawler.logger.info('Find a similar movie in douban <%s> <%s> <%s>' % (movie_title, similar_movie.get('title'), max_score))
+                else:
+                    IQIYICrawler.logger.warning('Cannot find any similar movie with douban <%s>' % movie_title)
 
             except PyMongoError, e:
                 IQIYICrawler.logger.error('Mongodb error <%s> when manually matching movie <%s>' % (e, movie_title))
