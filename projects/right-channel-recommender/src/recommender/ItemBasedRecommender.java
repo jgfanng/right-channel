c00910b5d0d9ef5774c0030b3d42778e8c7cde83
yapianyu <jgfang725@gmail.com>
2013-05-27 18:27:12 +0800

    att

--- projects/right-channel-recommender/src/recommender/ItemBasedRecommender.java
+++ projects/right-channel-recommender/src/recommender/ItemBasedRecommender.java
@@ -2,36 +2,56 @@ package recommender;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.model.mongodb.MongoDBDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.bson.types.ObjectId;
+
+import com.mongodb.MongoException;
 
 public class ItemBasedRecommender {
 
-   
-    public static void main(String[] args) throws TasteException {
-        try {
-            DataModel model=new FileDataModel(new File("/home/yapianyu/Desktop/intro.csv"));
-            UserSimilarity similarity=new PearsonCorrelationSimilarity(model);
-            UserNeighborhood neighborhood=new NearestNUserNeighborhood(2,similarity,model);
-            Recommender recommender=new GenericUserBasedRecommender(model,neighborhood,similarity);
-            List<RecommendedItem> recommendations=recommender.recommend(1,1);
-            for(RecommendedItem recommendation : recommendations)
-                System.out.println(recommendation);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+	public static void main(String[] args) throws TasteException,
+			UnknownHostException, MongoException {
+		MongoDBDataModel model = new MongoDBDataModel("127.0.0.1", 27017,
+				"right-channel", "ratings", false, false, null, "user_id",
+				"movie_id", "rating", MongoDBDataModel.DEFAULT_MONGO_MAP_COLLECTION);
+		ItemSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		Recommender recommender = new GenericItemBasedRecommender(model,
+				similarity);
+		List<RecommendedItem> recommendations = recommender.recommend(
+				Long.parseLong(model.fromIdToLong(
+						new ObjectId("51a0c3b3862f09377d7148d3").toStringMongod(), false)), 10);
+		for (RecommendedItem recommendation : recommendations)
+			System.out.println(recommendation);
+		// try {
+		// DataModel model = new FileDataModel(new File("intro.csv"));
+		// UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+		// UserNeighborhood neighborhood = new NearestNUserNeighborhood(5,
+		// similarity, model);
+		// Recommender recommender = new GenericUserBasedRecommender(model,
+		// neighborhood, similarity);
+		// List<RecommendedItem> recommendations = recommender.recommend(5, 1);
+		// for (RecommendedItem recommendation : recommendations)
+		// System.out.println(recommendation);
+		// } catch (IOException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 
-    }
+	}
 
 }
