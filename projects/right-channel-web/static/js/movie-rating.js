326c07c15386a892cbd2c0f0a9accc912340745c
yapianyu <jgfang725@gmail.com>
2013-07-02 19:08:33 +0800

    att

--- projects/right-channel-web/static/js/movie-rating.js
+++ projects/right-channel-web/static/js/movie-rating.js
@@ -50,24 +50,30 @@ $(function() {
         var html = '';
         for (var i = 0; i < movies.length; i++) {
             var movie = movies[i];
-            var posterImg = '<a href="/movie/{0}" target="_blank"><img src="http://img3.douban.com/lpic/s11356673.jpg" class="poster-medium"></a>'.format(movie._id);
-            var topBadge = '<div class="poster-badge poster-badge-top"><div class="caption">{0}</div></div>'.format(movie.title);
-            var bottomBadge = '<div class="poster-badge poster-badge-bottom hide">'
-                +                 '<div class="user-rating star-rating">'
-                +                     '<a class="r1" title="给电影打0.5分" data-rating="0.5"></a>'
-                +                     '<a class="r2" title="给电影打1分" data-rating="1"></a>'
-                +                     '<a class="r3" title="给电影打1.5分" data-rating="1.5"></a>'
-                +                     '<a class="r4" title="给电影打2分" data-rating="2"></a>'
-                +                     '<a class="r5" title="给电影打2.5分" data-rating="2.5"></a>'
-                +                     '<a class="r6" title="给电影打3分" data-rating="3"></a>'
-                +                     '<a class="r7" title="给电影打3.5分" data-rating="3.5"></a>'
-                +                     '<a class="r8" title="给电影打4.0分" data-rating="4"></a>'
-                +                     '<a class="r9" title="给电影打4.5分" data-rating="4.5"></a>'
-                +                     '<a class="r10" title="给电影打5分" data-rating="5"></a>'
-                +                '</div>'
-                +            '</div>';
-            var interestBtn = '<a title="想看" class="interest-btn interest-btn-close hide" data-interest-type="wish" href="javascript:void(0)"></a>';
-            html += '<li><div class="poster-thumbnail" data-movie-id="{0}">{1}{2}{3}{4}</div></li>'.format(movie._id, posterImg, topBadge, bottomBadge, interestBtn);
+            html += '<li>'
+                +       '<div class="poster-thumbnail" data-movie-id="{0}">'.format(movie._id)
+                +           '<img class="poster-medium" src="http://img3.douban.com/lpic/s11356673.jpg">'
+                +           '<a class="poster-mask hide" href="/movie/{0}" title="{1}" target="_blank"></a>'.format(movie._id, movie.title)
+                +           '<div class="poster-badge poster-badge-top">'
+                +               '<div class="caption">{0}</div>'.format(movie.title)
+                +           '</div>'
+                +           '<div class="poster-badge poster-badge-middle hide">'
+                +                '<div class="user-rating star-rating-large">'
+                +                    '<a class="r1" title="给电影打0.5分" data-rating="0.5"></a>'
+                +                    '<a class="r2" title="给电影打1分" data-rating="1"></a>'
+                +                    '<a class="r3" title="给电影打1.5分" data-rating="1.5"></a>'
+                +                    '<a class="r4" title="给电影打2分" data-rating="2"></a>'
+                +                    '<a class="r5" title="给电影打2.5分" data-rating="2.5"></a>'
+                +                    '<a class="r6" title="给电影打3分" data-rating="3"></a>'
+                +                    '<a class="r7" title="给电影打3.5分" data-rating="3.5"></a>'
+                +                    '<a class="r8" title="给电影打4.0分" data-rating="4"></a>'
+                +                    '<a class="r9" title="给电影打4.5分" data-rating="4.5"></a>'
+                +                    '<a class="r10" title="给电影打5分" data-rating="5"></a>'
+                +               '</div>'
+                +           '</div>'
+                +           '<a title="没看过" class="char-btn char-btn-close hide" data-interest-type="wish" href="javascript:void(0)">&times;</a>';
+                +       '</div>'
+                +   '</li>';
         }
         $('.poster-thumbnails').append(html);
     }
@@ -79,7 +85,7 @@ $(function() {
         }
     });
 
-    $('.poster-thumbnails').on('click', '.star-rating', function(event) {
+    $('.poster-thumbnails').on('click', '.star-rating-large', function(event) {
         var movieId = $(this).closest('.poster-thumbnail').attr('data-movie-id');
         var rating = $(event.target).attr('data-rating');
         $.ajax({
@@ -100,33 +106,16 @@ $(function() {
                 window.location.href = '/login';
             }
         });
-    }).on('click', 'a[data-interest-type]', function() {
-        var movieId = $(this).closest('.poster-thumbnail').attr('data-movie-id');
-        var interestType = $(this).attr('data-interest-type');
-        $.ajax({
-            type : 'POST',
-            url : '/api/movie/{0}/interest'.format(movieId),
-            data : {
-                type: interestType
-            },
-            context : $(this).closest('.poster-thumbnail').closest("li")
-        }).done(function() {
-            $(this).fadeOut(function() {
-                loadNextRating();
-            });
-        }).fail(function(jqXHR) {
-            if (jqXHR.status == 401) { // unauthorized
-                $.cookie.raw = true;
-                $.cookie('next', window.location.href);
-                window.location.href = '/login';
-            }
-        });
     }).on('mouseenter', '.poster-thumbnail', function() {
-        $(this).find('.poster-badge-bottom').stop().fadeIn();
-        $(this).find('.interest-btn').stop().show();
+        $(this).find('.poster-badge-top').hide();
+        $(this).find('.poster-mask').show();
+        $(this).find('.poster-badge-middle').show();
+        $(this).find('.char-btn').show();
     }).on('mouseleave', '.poster-thumbnail', function() {
-        $(this).find('.poster-badge-bottom').stop().hide();
-        $(this).find('.interest-btn').stop().hide();
+        $(this).find('.poster-badge-top').show();
+        $(this).find('.poster-mask').hide();
+        $(this).find('.poster-badge-middle').hide();
+        $(this).find('.char-btn').hide();
     });
     
     loadNextPage();
--- projects/right-channel-web/static/js/movie-rcmd.js
+++ projects/right-channel-web/static/js/movie-rcmd.js
@@ -48,30 +48,36 @@ $(function() {
         var html = '';
         for (var i = 0; i < movies.length; i++) {
             var movie = movies[i];
-            var posterImg = '<a href="/movie/{0}" target="_blank"><img src="http://img3.douban.com/lpic/s11356673.jpg" class="poster-medium"></a>'.format(movie._id);
-            var topBadge = '<div class="poster-badge poster-badge-top"><div class="caption">{0}</div></div>'.format(movie.title);
-            var bottomBadge = '<div class="poster-badge poster-badge-bottom">'
-                +                 '<div class="douban-rating">'
-                +                     '豆瓣评分: {0}'.format(('douban' in movie && 'rating' in movie.douban) ? movie.douban.rating : '未知')
-                +                     '<a class="pull-right"><i class="icon-download-alt icon-white"></i></a>'
-                +                     ('resources' in movie && 'online' in movie.resources ? '<a class="pull-right"><i class="icon-play-circle icon-white"></i></a>' : '')
-                +                 '</div>'
-                +                 '<div class="user-rating star-rating hide">'
-                +                     '<a class="r1" title="给电影打0.5分" data-rating="0.5"></a>'
-                +                     '<a class="r2" title="给电影打1分" data-rating="1"></a>'
-                +                     '<a class="r3" title="给电影打1.5分" data-rating="1.5"></a>'
-                +                     '<a class="r4" title="给电影打2分" data-rating="2"></a>'
-                +                     '<a class="r5" title="给电影打2.5分" data-rating="2.5"></a>'
-                +                     '<a class="r6" title="给电影打3分" data-rating="3"></a>'
-                +                     '<a class="r7" title="给电影打3.5分" data-rating="3.5"></a>'
-                +                     '<a class="r8" title="给电影打4.0分" data-rating="4"></a>'
-                +                     '<a class="r9" title="给电影打4.5分" data-rating="4.5"></a>'
-                +                     '<a class="r10" title="给电影打5分" data-rating="5"></a>'
+            html += '<li>'
+                +       '<div class="poster-thumbnail" data-movie-id="{0}">'.format(movie._id)
+                +           '<img class="poster-medium" src="http://img3.douban.com/lpic/s11356673.jpg">'
+                +           '<a class="poster-mask hide" href="/movie/{0}" title="{1}" target="_blank"></a>'.format(movie._id, movie.title)
+                +           '<div class="poster-badge poster-badge-top">'
+                +               '<div class="caption">{0}</div>'.format(movie.title)
+                +           '</div>'
+                +           '<div class="poster-badge poster-badge-bottom">'
+                +               '<div class="douban-rating">'
+                +                    '豆瓣评分: {0}'.format(('douban' in movie && 'rating' in movie.douban) ? movie.douban.rating : '未知')
+                +                    '<a class="pull-right"><i class="icon-download-alt icon-white"></i></a>'
+                +                    ('resources' in movie && 'online' in movie.resources ? '<a class="pull-right"><i class="icon-play-circle icon-white"></i></a>' : '')
                 +                '</div>'
-                +            '</div>';
-            var interestBtn = '<a title="想看" class="interest-btn interest-btn-wish hide" data-interest-type="wish" href="javascript:void(0)"></a>'
-                +             '<a title="没兴趣" class="interest-btn interest-btn-dislike hide" data-interest-type="dislike" href="javascript:void(0)"></a>';
-            html += '<li><div class="poster-thumbnail" data-movie-id="{0}">{1}{2}{3}{4}</div></li>'.format(movie._id, posterImg, topBadge, bottomBadge, interestBtn);
+                +                '<div class="user-rating star-rating hide">'
+                +                    '<a class="r1" title="给电影打0.5分" data-rating="0.5"></a>'
+                +                    '<a class="r2" title="给电影打1分" data-rating="1"></a>'
+                +                    '<a class="r3" title="给电影打1.5分" data-rating="1.5"></a>'
+                +                    '<a class="r4" title="给电影打2分" data-rating="2"></a>'
+                +                    '<a class="r5" title="给电影打2.5分" data-rating="2.5"></a>'
+                +                    '<a class="r6" title="给电影打3分" data-rating="3"></a>'
+                +                    '<a class="r7" title="给电影打3.5分" data-rating="3.5"></a>'
+                +                    '<a class="r8" title="给电影打4.0分" data-rating="4"></a>'
+                +                    '<a class="r9" title="给电影打4.5分" data-rating="4.5"></a>'
+                +                    '<a class="r10" title="给电影打5分" data-rating="5"></a>'
+                +               '</div>'
+                +           '</div>'
+                +           '<a title="想看" class="char-btn char-btn-plus hide" data-interest-type="wish" href="javascript:void(0)">&plus;</a>'
+                +           '<a title="没兴趣" class="char-btn char-btn-minus hide" data-interest-type="dislike" href="javascript:void(0)">&minus;</a>'
+                +       '</div>'
+                +   '</li>';
         }
         $('.poster-thumbnails').append(html);
     }
@@ -126,13 +132,15 @@ $(function() {
             }
         });
     }).on('mouseenter', '.poster-thumbnail', function() {
-        $(this).find('.douban-rating').stop().hide();
-        $(this).find('.user-rating').stop().fadeIn();
-        $(this).find('.interest-btn').stop().show();
+        $(this).find('.poster-mask').show();
+        $(this).find('.douban-rating').hide();
+        $(this).find('.user-rating').show();
+        $(this).find('.char-btn').show();
     }).on('mouseleave', '.poster-thumbnail', function() {
-        $(this).find('.user-rating').stop().hide();
-        $(this).find('.interest-btn').stop().hide();
-        $(this).find('.douban-rating').stop().fadeIn();
+        $(this).find('.poster-mask').hide();
+        $(this).find('.douban-rating').show();
+        $(this).find('.user-rating').hide();
+        $(this).find('.char-btn').hide();
     });
     
     loadNextPage();
